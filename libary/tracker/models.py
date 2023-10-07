c2e11052c9db98e27e31d9d94f175cee08e471c7
Dest1ni <vilo76671@gmail.com>
2023-10-07 19:02:42 +0300

    git init

---
+++ libary/tracker/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    author = models.ForeignKey(Author,on_delete=models.PROTECT)
+
+class Reader(models.Model):
+    name = models.CharField(max_length=255)
+
+class ReaderBookRelationship(models.Model):
+    reader = models.ForeignKey(Reader,on_delete=models.DO_NOTHING)
+    book = models.ForeignKey(Book,on_delete=models.DO_NOTHING)
+    give_date = models.DateTimeField(auto_now_add = True)
+    return_date = models.DateTimeField(null = True)
